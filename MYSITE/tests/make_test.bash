7475a653e0f908cc629cc86f04b6f46d7b777105
Anthony Bradley <anthony.richard.bradley@gmail.com>
2015-03-13 11:44:54 +0000

    Added a test bash file

---
+++ MYSITE/tests/make_test.bash
@@ -0,0 +1,16 @@
+OUTPUT="$(docker run -d=true -p 80:8000 -e "SERVER_NAME=localhost" -e "SERVER_PORT=8000" abradle/rdkitserver)"
+echo STARTED SERVER
+echo WAITING TO CONNECT
+echo 1
+sleep 1
+echo 2
+sleep 1
+echo 3
+sleep 1
+echo 4
+sleep 1
+echo 5
+sleep 1
+python cluster_tests.py
+python screen_tests.py
+docker kill $OUTPUT
