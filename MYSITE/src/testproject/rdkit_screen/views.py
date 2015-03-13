18f2ad799aa4dd3dcf350be81321e6580c719e4e
Anthony Bradley <anthony.richard.bradley@gmail.com>
2015-03-13 13:07:14 +0000

    M

--- MYSITE/src/testproject/rdkit_screen/views.py
+++ MYSITE/src/testproject/rdkit_screen/views.py
@@ -19,7 +19,8 @@ def screen(request):
     my_json = ast.literal_eval(my_j)
     if "SMILES" in my_json:
         smiles = my_json["SMILES"]
-        scr_mols = [{"RDMOL": Chem.MolFromSmiles(str(x)} for x in str(smiles).split(".")]
+        
+        scr_mols = [{"RDMOL": Chem.MolFromSmiles(str(smiles)}]# for x in str(smiles).split(".")]
     else:
         return HttpResponse("You must state a SMILES")
 
