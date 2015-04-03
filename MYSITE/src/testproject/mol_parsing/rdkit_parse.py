716ca3f8e46ec1718275a66299065b94e2e7f96e
Anthony Bradley <anthony.richard.bradley@gmail.com>
2015-04-03 16:58:31 +0000

    Added sanifix and fixed parsing of molecules

--- MYSITE/src/testproject/mol_parsing/rdkit_parse.py
+++ MYSITE/src/testproject/mol_parsing/rdkit_parse.py
@@ -1,7 +1,7 @@
 #Series of functions to parse molecules based on RDKit
 import sys
 from rdkit import Chem
-
+from sanifix import fix_mol
 def parse_mol_simple(my_type, txt):
     """Function to parse individual mols given a type"""
     if my_type == "mol":
@@ -11,16 +11,20 @@ def parse_mol_simple(my_type, txt):
             mol = Chem.MolFromMolBlock(txt)
         if mol is None:
             mol = Chem.MolFromMolBlock("\n".join(txt.split("\n")[1:]))
+        # Now try to do sanidfix
         if mol is None:
-            mol = Chem.MolFromMolBlock(txt, strictParsing=False)
+            mol = fix_mol(Chem.MolFromMolBlock(txt, False))
+        # Annd again
         if mol is None:
-            mol = Chem.MolFromMolBlock(txt.strip(), strictParsing=False)
+            mol = fix_mol(Chem.MolFromMolBlock(txt.strip(), False))
     elif my_type == "smiles":
         # Assumes that smiles is the first column
         mol = Chem.MolFromSmiles(txt.split()[0])
     elif my_type == "inchi":
         # Assumes that INCHI is the first column
         mol = Chem.MolFromInchi(my_txt.split()[0], my_vals)
+    if mol is None:
+        print txt
     return mol
 
 
